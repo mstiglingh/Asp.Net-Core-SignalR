e1a50c3c44933e8d3e395953afc5ebb945fff792
Meyer Stiglingh <mstiglingh@esri-southafrica.com>
2020-09-23 11:13:25 +0200

    Add Group Example

---
+++ Asp.Net-Core-SignalR/Asp.Net-Core-SignalR With Groups/Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Asp.Net_Core_SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.Group("1").SendAsync("ReceiveMessage", "System", "This is only for group 1");
+            await Clients.Group("2").SendAsync("ReceiveMessage", "System", "This is only for group 2");
+        }
+        public override async Task OnConnectedAsync()
+        {
+            var group = Context.GetHttpContext().Request.Query["token"];
+
+            string value = !string.IsNullOrEmpty(group.ToString()) ? group.ToString() : "default";
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, value);
+            await base.OnConnectedAsync();
+        }
+    }
+}
---
+++ Asp.Net-Core-SignalR/Asp.Net-Core-SignalR With Groups/Pages/Index2.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Asp.Net_Core_SignalR.Pages
+{
+    public class Inde2xModel : PageModel
+    {
+        private readonly ILogger<Inde2xModel> _logger;
+
+        public Inde2xModel(ILogger<Inde2xModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
